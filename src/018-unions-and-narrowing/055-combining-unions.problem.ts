6bcaafe279e6193458aad954dbf3fe9a0c1f9694
Khanh Sy <sykhanh1996@gmail.com>
2024-08-02 18:08:03 +0700

    unions and narrowing

--- src/018-unions-and-narrowing/055-combining-unions.problem.ts
+++ src/018-unions-and-narrowing/055-combining-unions.problem.ts
@@ -2,13 +2,13 @@ import { Equal, Expect } from "@total-typescript/helpers";
 
 type HttpCode = "400" | "401" | "404" | "500" | "200" | "201" | "204";
 
-const handleErrorCase = (code: string) => {
+const handleErrorCase = (code: "400" | "401" | "404" | "500") => {
   // An imaginary function where we only handle the errors
 
   type test = Expect<Equal<typeof code, "400" | "401" | "404" | "500">>;
 };
 
-const handleSuccessCase = (code: string) => {
+const handleSuccessCase = (code: "200" | "201" | "204") => {
   // An imaginary function where we only handle the success cases
 
   type test = Expect<Equal<typeof code, "200" | "201" | "204">>;
